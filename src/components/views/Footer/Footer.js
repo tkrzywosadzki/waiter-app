16ce9ffd2351a511919a7b449297efbc00f4fe0a
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-06-18 22:22:50 +0200

    add NavBar and Footer

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="text-center text-muted mt-4 mb-4">
+            <p>Copyright &copy; WaiterApp 2024</p>
+        </footer>
     );
 };
 
--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,8 +1,17 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom';
+
+
 const NavBar = () => {
-    return (
-        <div>
-            NavBar
-        </div>
+    return ( 
+        <Navbar bg='primary' variant='dark' expand='lg' className='mt-4 mb-4 rounded'>
+            <Container>
+                <Navbar.Brand as={NavLink} to="/">Waiter.app</Navbar.Brand>
+                <Nav className='d-flex justify-content-end'>
+                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
 
