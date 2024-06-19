d3d4e8adeda6bee39e7a80447cbb4f633758c105
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-06-19 18:30:41 +0200

    add update tables from server to local

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import NotFound from './components/pages/NotFound/NotFound';
 import TablePage from './components/pages/TablePage/TablePage';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import { useDispatch } from 'react-redux';
+import { fetchTables } from './redux/tablesRedux';
+import { useEffect } from 'react';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+
   return (
     <main>
       <Container>
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -2,10 +2,22 @@
 
 // actions
 const createActionName = actionName => `app/tables/${actionName}`;
+const UPDATE_TABLES = createActionName('UPDATE_TABLES');
 
 // action creators
+export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
+export const fetchTables = () => {
+    return (dispatch) => {
+        fetch('http://localhost:3131/tables')
+            .then(res => res.json())
+            .then(tables => dispatch(updateTables(tables)));
+    }
+};
+
 const tablesReducer = (statePart = [], action) => {
     switch (action.type) {
+        case UPDATE_TABLES:
+            return [...action.payload]
         default:
             return statePart;
     };
